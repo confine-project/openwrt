b57e7d0a6e2ebdce0f6f4adac0f6d42592c8a589
Axel Neumann <neumann@cgws.de>
2012-09-30 13:28:54 +0200

    realview/lxc_host/target.mk: add FEATURES:=squashfs jffs2 ext4 pci usb gpio

--- target/linux/realview/lxc_host/target.mk
+++ target/linux/realview/lxc_host/target.mk
@@ -1,5 +1,6 @@
 BOARDNAME:=Linux Container (LXC) Host System Target
-FEATURES:=fpu ramdisk
+
+FEATURES:=squashfs jffs2 ext4 pci usb gpio
 
 define Target/Description
 	Build firmware images suitable to host Linux container (LXC) guests
