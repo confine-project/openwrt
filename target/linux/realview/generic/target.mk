f8ecb5bdafebd9d26443e7aa4fbc1f52c6cf65ae
Axel Neumann <neumann@cgws.de>
2012-12-05 07:58:25 +0100

    Revert lxc_host target modifications. LXC support now comes as kernel build options cgroup and namespaces (Global build settings)
    Reverted commits are:
    
    Revert "[x86] add experimental, optimized lxc_host_alix2 subtarget for alix2 boards"
    
    This reverts commit 9d1a75ef5998fb6ccc0e0092fa15329a92b6e1e3.
    
    Revert "x86/lxc_host: extend config-3.3 and target.mk with x86/generic/ features"
    
    This reverts commit d0a62c2adb87a34fc22c9034d755523975977d39.
    
    	deleted:    target/linux/x86/lxc_host/config-3.3
    	new file:   target/linux/x86/lxc_host/config-default
    	modified:   target/linux/x86/lxc_host/target.mk
    
    Revert "realview/lxc_host/target.mk: add FEATURES:=squashfs jffs2 ext4 pci usb gpio"
    
    This reverts commit b57e7d0a6e2ebdce0f6f4adac0f6d42592c8a589.
    
    	modified:   target/linux/realview/lxc_host/target.mk
    
    Revert "realview/lxc_host: fix config-default typo"
    
    This reverts commit fe68e1860884e4c9c4a3ebd19b1393513ae7c143.
    
    	modified:   target/linux/realview/lxc_host/config-default
    
    Revert "ar71xx/lxc_host: fix config-default typo"
    
    This reverts commit bff3775f7b1cf7c8d298b5bf50c926e769a33b2c.
    
    	modified:   target/linux/ar71xx/lxc_host/config-default
    
    Revert "[realview] add experimental lxc_host subtarget for arm"
    
    This reverts commit a0fb307d4af657eb0dd52e397260485062ad588f.
    
    	modified:   target/linux/realview/Makefile
    	deleted:    target/linux/realview/generic/target.mk
    	deleted:    target/linux/realview/lxc_host/config-default
    	deleted:    target/linux/realview/lxc_host/target.mk
    
    Revert "[ar71xx] add experimental lxc_host subtarget for mips"
    
    This reverts commit 14bae300c1c6429caed8a4c30103937e031169d2.
    
    	modified:   target/linux/ar71xx/Makefile
    	deleted:    target/linux/ar71xx/lxc_host/config-default
    	deleted:    target/linux/ar71xx/lxc_host/profiles/00-default.mk
    	deleted:    target/linux/ar71xx/lxc_host/target.mk
    
    Revert "[lxc_host] explicitely enable CONFIG_NAMESPACES support"
    
    This reverts commit 8cf958979814505efd03f67c301cd9e7f76afff5.
    
    	modified:   target/linux/x86/lxc_host/config-default
    
    Revert "[lxc_host] add missing kernel config symbol"
    
    This reverts commit e230504727610c86e4acc0f76cacbd18700656a7.
    
    	modified:   target/linux/x86/lxc_host/config-default
    
    Revert "[x86] add a lxc_host subtarget with required kernel config overrides to support Linux Containers (LXC)"
    
    This reverts commit a973784cfa931bd7f9844cd6eff774501a1df170.
    
    	modified:   target/linux/x86/Makefile
    	deleted:    target/linux/x86/lxc_host/config-default
    	deleted:    target/linux/x86/lxc_host/target.mk

--- target/linux/realview/generic/target.mk
+++
@@ -1,6 +0,0 @@
-BOARDNAME:=Generic Realview target
-FEATURES:=fpu ramdisk
-
-define Target/Description
-	Build firmware images suitable for quem
-endef
--- target/linux/realview/lxc_host/target.mk
+++
@@ -1,7 +0,0 @@
-BOARDNAME:=Linux Container (LXC) Host System Target
-
-FEATURES:=squashfs jffs2 ext4 pci usb gpio
-
-define Target/Description
-	Build firmware images suitable to host Linux container (LXC) guests
-endef
