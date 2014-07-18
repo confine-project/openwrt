a5f5b17a708e4e36b974c21f6b17adc613c4465a
Axel Neumann <neumann@cgws.de>
2014-07-18 06:57:50 +0000

    cmake.mk: extend CMAKE_PREFIX_PATH with /usr (needed for dlep/oonf-api)

--- include/cmake.mk
+++ include/cmake.mk
@@ -55,7 +55,7 @@ define Build/Configure/Default
 			-DCMAKE_STRIP=: \
 			-DCMAKE_INSTALL_PREFIX=/usr \
 			-DDL_LIBRARY=$(STAGING_DIR) \
-                        -DCMAKE_PREFIX_PATH=$(STAGING_DIR) \
+                        -DCMAKE_PREFIX_PATH=$(STAGING_DIR)/usr \
 			$(CMAKE_OPTIONS) \
 		$(CMAKE_SOURCE_DIR) \
 	)
